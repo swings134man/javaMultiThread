3026683352a3b14d70f5148265e9676b736d71c0
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2022-06-17 18:06:51 +0900

    priority 설정

--- multithreading/src/MultiThread1.java
+++ multithreading/src/MultiThread1.java
@@ -13,6 +13,10 @@ public class MultiThread1 {
 
         thread.setName("new Thread nm1"); // Thread 객체 이름 설정.
 
+        // 우선 순위, 시스템이 busy 시 우선순위가 높은 Thread에게 우선적 CPU 할당.
+        // 1 ~ 10 설정값 존재.
+        thread.setPriority(Thread.MAX_PRIORITY);
+
         System.out.println("Thread 객체 : " + thread.currentThread().getName());
         thread.start();
         System.out.println("Thread 객체  after : " + thread.currentThread().getName());
